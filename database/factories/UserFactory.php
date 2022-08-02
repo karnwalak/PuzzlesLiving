7c1de1fc7b833c25f653a7b6e16aebb6500ce64f
karnwalak <karnwalakshay7@gmail.com>
2022-08-02 07:23:36 +0530

    NA

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,8 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'type' => 'admin',
+            'password' => '$2a$12$WMwV600jmnqaP/E/kYBzSeDMj8owi9dNWawnxB72oPixa.5pY1sB.', // password
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => '$2a$12$WMwV600jmnqaP/E/kYBzSeDMj8owi9dNWawnxB72oPixa.5pY1sB.',
+        ]);
     }
 }
